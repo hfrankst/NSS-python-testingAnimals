5e3302411d41289bea0b444922b066be800acb49
Harper Frankstone <Harper Frankstone>
2017-04-11 13:25:11 -0500

    refactored a little bit and made it better....thanks live coding

--- README.md
+++ README.md
@@ -15,15 +15,16 @@ As a team, we'll be building unit test coverage for all the functionality of the
 
 ### Instructions
 
-Write test cases to verify the I/O of the following methods of Animal and Dog.
+Write test cases to verify the I/O of the following methods of ```Animal``` and ```Dog```.
 
-In the test class' setUpClass() method, create an instance of Animal and Dog.
-Animal object has the correct name property.
-Set a species and verify that the object property of species has the correct value.
-Invoking the walk() method set the correct speed on the both objects.
-The animal object is an instance of Animal.
-The dog object is an instance of Dog.
-Test Discovery
+1. In the test class' ```setUpClass()``` method, create an instance of ```Animal``` and ```Dog```.
+2. Animal object has the correct name property.
+3.Set a species and verify that the object property of species has the correct value.
+4. Invoking the walk() method set the correct speed on the both objects.
+5. The animal object is an instance of Animal.
+6. The dog object is an instance of Dog.
+
+## Test Discovery
 
 Read the Test Discovery section of the Python docs. It explains how you can run all tests inside a directory. This allows you to split your unit test suite into many, smaller, more maintainable modules, and the use a pattern matcher to find tests in all matching files.
 
--- testAnimals.py
+++ testAnimals.py
@@ -1,41 +1,46 @@
 import unittest
-from animals import Animal, Dog
+from animals import *
 
 class TestAnimals(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(self): 
 		'''this is a special python class required for unit testing. The @classmethod above it allows the setUpClass to be used again without making new instances of it'''
-		self.animal = Animal('Dog')
+		self.animal = Animal('Buddy', 'Panda')
 		self.dog = Dog('Fido')
 
 
-	def test_animal_name(self):
+	def test_animal_name_is_correct(self):
 		'''this is testing whether the Animal object has a property of name. This test is done by passing a name to Animal and then asserting that it is not equal to 'none' aka that it does exist'''
-		ani = Animal('Fido')
-		self.assertIsNotNone(ani.name)
+		self.assertEqual(self.animal.name, 'Buddy')
 
-	def test_species(self):
+	def test_animal_species(self):
 		'''testing the value of the species object on the potato instance has the correct value, which is the second parameter in the assertEqual method. Addtionally, this tests that there is a species attribute on the Animal object'''
-		potato = Animal(species='Dog')
-		self.assertEqual(potato.species, 'Dog')
+		self.assertEqual(self.animal.species, 'Panda')
+
+	def test_dog_species(self):
+		'''testing the value of the species object on the potato instance has the correct value, which is the second parameter in the assertEqual method. Addtionally, this tests that there is a species attribute on the Animal object'''
+		self.assertEqual(self.dog.species, 'Dog')
 
 	def test_speed(self):
 		'''setting the speed on both Dog and Animal, by invoking the walk method that is on the Animal object'''
-		potato = Animal()
-		potato.set_legs(4)
-		potato.walk()
-		self.assertEqual(potato.speed, 0.4)
+		legs = 4
+		self.animal.set_legs(legs)
+		self.animal.walk()
 
-	def test_animal_instance(self):
+		self.dog.set_legs(legs)
+		self.dog.walk()
+
+		self.assertEqual(self.animal.speed, 0.4)
+		self.assertEqual(self.dog.speed, 0.8)
+
+	def test_animal_instance_is_correct_type(self):
 		'''testing to make sure that the animal object is an instance of animal object'''
-		new_animal = Animal()
-		self.assertIsInstance(new_animal, Animal)
+		self.assertIsInstance(self.animal, Animal)
 
-	def test_dog_instance(self):
+	def test_dog_instance_is_correct_type(self):
 		'''testing to make sure that the dog object is actually an instance of the dog object'''
-		new_dog = Dog('Fido')
-		self.assertIsInstance(new_dog, Dog)
+		self.assertIsInstance(self.dog, Dog)
 
 
 #the next two lines fire the above functions and tests
